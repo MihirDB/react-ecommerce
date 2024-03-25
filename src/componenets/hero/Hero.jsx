bff69e416cde65589f235542fad30bf32961b51f
MihirDB <mihirdb3@gmail.com>
2024-03-25 13:40:56 +0000

    Animation added

--- src/componenets/hero/Hero.jsx
+++ src/componenets/hero/Hero.jsx
@@ -3,8 +3,11 @@ import css from "./Hero.module.css";
 import HeroImg from "../../assets/hero.png";
 import { RiShoppingBagFill } from "react-icons/ri";
 import { BsArrowRight } from "react-icons/bs";
+import { motion } from "framer-motion";
 
 export default function Hero() {
+    const transition = { duration: 3, type: "spring" };
+
     return (
         <div className={css.container}>
             <div className={css.h_sides}>
@@ -18,9 +21,26 @@ export default function Hero() {
             </div>
 
             <div className={css.wrapper}>
-                <div className={css.blueCircle}></div>
-                <img src={HeroImg} alt="" width={600} />
-                <div className={css.cart2}>
+                <motion.div
+                    initial={{ bottom: "2rem" }}
+                    whileInView={{ bottom: "0rem" }}
+                    className={css.blueCircle}
+                    transition={transition}
+                ></motion.div>
+                <motion.img
+                    transition={transition}
+                    initial={{ bottom: "-2rem" }}
+                    whileInView={{ bottom: "0rem" }}
+                    src={HeroImg}
+                    alt=""
+                    width={600}
+                />
+                <motion.div
+                    transition={transition}
+                    initial={{ right: "4%" }}
+                    whileInView={{ right: "2%" }}
+                    className={css.cart2}
+                >
                     <RiShoppingBagFill />
 
                     <div className={css.signup}>
@@ -29,7 +49,7 @@ export default function Hero() {
                             <BsArrowRight />
                         </div>
                     </div>
-                </div>
+                </motion.div>
             </div>
             <div className={css.h_sides}>
                 <div className={css.traffic}>
